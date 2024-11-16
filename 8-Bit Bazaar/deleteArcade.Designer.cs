deb34a90f051e085557efd7607b46e2c26e78d3e
Zadif <zadifmustafa93@gmail.com>
2024-11-16 09:20:07 +0500

    Solved errors and added new ones

--- 8-Bit Bazaar/deleteArcade.Designer.cs
+++ 8-Bit Bazaar/deleteArcade.Designer.cs
@@ -38,11 +38,11 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Bahnschrift SemiLight Condensed", 20.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label2.Location = new System.Drawing.Point(16, 138);
+            this.label2.Location = new System.Drawing.Point(59, 143);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(432, 34);
+            this.label2.Size = new System.Drawing.Size(321, 34);
             this.label2.TabIndex = 23;
-            this.label2.Text = "Are you sure you want to delete this arcade?";
+            this.label2.Text = "Are you sure you want to delete?";
             // 
             // button4
             // 
@@ -51,37 +51,37 @@
             this.button4.FlatAppearance.BorderSize = 2;
             this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Purple;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button4.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.button4.Location = new System.Drawing.Point(176, 205);
+            this.button4.Location = new System.Drawing.Point(154, 230);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(112, 48);
             this.button4.TabIndex = 22;
             this.button4.Text = "Delete";
             this.button4.UseVisualStyleBackColor = false;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Bahnschrift SemiLight Condensed", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Location = new System.Drawing.Point(91, 25);
+            this.label1.Location = new System.Drawing.Point(143, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(273, 64);
+            this.label1.Size = new System.Drawing.Size(139, 64);
             this.label1.TabIndex = 21;
-            this.label1.Text = "Delete Arcade";
+            this.label1.Text = "Delete";
             // 
             // deleteArcade
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(12)))), ((int)(((byte)(40)))));
-            this.ClientSize = new System.Drawing.Size(460, 284);
+            this.ClientSize = new System.Drawing.Size(446, 329);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.label1);
             this.Name = "deleteArcade";
-            this.Text = "Delete Arcade";
+            this.Text = "30, 12, 40";
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- 8-Bit Bazaar/deleteArcade.cs
+++ 8-Bit Bazaar/deleteArcade.cs
@@ -16,24 +16,34 @@ namespace _8_Bit_Bazaar
     {
         static MongoClient client = new MongoClient();
         static IMongoDatabase db = client.GetDatabase("8BitBazar");
-        static IMongoCollection<Arcade> collection = db.GetCollection<Arcade>("arcades");
+        static IMongoCollection<Arcade> arcadecollection = db.GetCollection<Arcade>("arcades");
+        static IMongoCollection<User> collection = db.GetCollection<User>("users");
         public deleteArcade()
         {
             InitializeComponent();
-            
+
         }
         public deleteArcade(Arcade arc)
         {
             InitializeComponent();
             button4.Click += (sender, e) =>
             {
-                collection.DeleteOne(s => s.Id == arc.Id);
+                arcadecollection.DeleteOne(s => s.Id == arc.Id);
                 MessageBox.Show("Arcade deleted");
+
+                
+                userInfo.user.Sell.Remove(arc.Id.ToString());
+
+                var filter = Builders<User>.Filter.Eq(u1 => u1.Id, userInfo.user.Id);
+                var update = Builders<User>.Update.Set(u1 => u1.Sell, userInfo.user.Sell);
+
+                collection.UpdateOne(filter, update);
+
                 editListing e3 = new editListing();
                 this.Hide();
                 e3.Show();
             };
         }
-       
+
     }
 }
